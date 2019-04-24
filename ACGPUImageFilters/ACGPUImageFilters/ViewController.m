d30669ba3ff29c47806e41a436bf5a028e51f776
albertchu <albertzyk@gmail.com>
2019-04-24 16:28:31 +0800

    prepare xproj
    creat empty proj

---
+++ ACGPUImageFilters/ACGPUImageFilters/ViewController.m
@@ -0,0 +1,26 @@
+//
+//  ViewController.m
+//  ACGPUImageFilters
+//
+//  Created by albert on 2019/4/24.
+//  Copyright © 2019 albert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+}
+
+
+@end
