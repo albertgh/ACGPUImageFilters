1d8cb9fabad32949cd825e0936761b3cdfad6b33
albertchu <albertzyk@gmail.com>
2019-04-26 18:13:55 +0800

    add ACImageMaskFilter

---
+++ ACGPUImageFilters/ACGPUImageFilters/filters/ACImageMaskFilter.h
@@ -0,0 +1,15 @@
+//
+//  ACImageMaskFilter.h
+//  ACGPUImageFilters
+//
+//  Created by albert on 2019/4/26.
+//  Copyright © 2019 albert. All rights reserved.
+//
+
+#import "GPUImageTwoInputFilter.h"
+
+
+@interface ACImageMaskFilter : GPUImageTwoInputFilter
+
+@end
+
